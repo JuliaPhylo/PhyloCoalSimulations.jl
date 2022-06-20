394716430be244a054bde78a16263b342299ee6e
fogg-uw <fogg@wisc.edu>
2022-06-20 17:03:34 -0500

    basic handling of hybrid nodes

--- src/simulatecoalescent_network.jl
+++ src/simulatecoalescent_network.jl
@@ -1,3 +1,12 @@
+"""
+    simulatecoalescent(net, nloci, nindividuals=1)
+
+Using species network `net`, simulate `nloci` gene trees
+with `nindividuals` from each species.
+
+Output: A length-`nloci` vector of gene trees
+
+"""
 function simulatecoalescent(net::PN.HybridNetwork, nloci, nindividuals=1)
     if isa(nindividuals, AbstractDict)
         # check if value types are Integers? || error("nindividuals should be integers")
@@ -24,6 +33,13 @@ function simulatecoalescent(net::PN.HybridNetwork, nloci, nindividuals=1)
         for nodei in nnodes:-1:1
             nn = net.nodes_changed[nodei]
             parentedgelist = PN.Edge[]
+
+            #this might be a more efficient way to check edges--we only have to run the loop once
+            #childedgelist = PN.Edge[]
+            #for e in nn.edge
+            #   PN.getChild(e) === nn ? push!(parentedgelist, e) : push!(childedgelist, e)
+            #end
+
             for e in nn.edge
                 PN.getChild(e) === nn || continue
                 push!(parentedgelist, e)
@@ -64,9 +80,28 @@ function simulatecoalescent(net::PN.HybridNetwork, nloci, nindividuals=1)
                 nextid = simulatecoal_onepopulation!(f, ee.length, nextid, ee.number)
             elseif nparents > 1
                 # partition random across all parent edges
+                # (in the gene trees: do we want to make degree-2 nodes with n.inCycle = the hybrid node
+                #  and new edges with e.inCycle = the appropriate hybrid parent?  not implemented right now)
+                gamma = Float64[]
+                for parent in parentedgelist
+                    append!(gamma, parent.gamma)
+                end
+                d = Distributions.Categorical(gamma)
+                for genetree in f
+                    roll=rand(d)
+                    whereto = parentedgelist[roll]
+                    #parentforest = edge2forest[whereto.number]
+                    #isempty(edge2forest[whereto.number]) ? edge2forest[whereto.number] = [genetree] : append!(edge2forest[whereto.number], genetree)
+                    append!(edge2forest[whereto.number], [genetree])
+                end
                 # run coalescent along each parent edge
+                for ee in parentedgelist
+                    nextid = simulatecoal_onepopulation!(edge2forest[ee.number], ee.length, nextid, ee.number)
+                end
             else # nparents = 0: infinite root population
-                nextid = simulatecoal_onepopulation!(f, Inf, nextid)
+                if length(f) > 1
+                    nextid = simulatecoal_onepopulation!(f, Inf, nextid) # length(f)>1 needed to avoid error "there should be 2 or more lineages at the start of the infinite root population" in simulatecoal_onepopulation!
+                end
                 rootnode = f[1].node[1]
                 genetreelist[ilocus] = convert2tree!(rootnode)
                 # may be write tree to output file?
