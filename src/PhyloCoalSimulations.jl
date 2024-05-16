a57a40b16c8727fb60a046a661db23bda1981dbf
Cecile Ane <cecileane@users.noreply.github.com>
2024-05-16 14:43:57 -0500

    Random dependency

--- src/PhyloCoalSimulations.jl
+++ src/PhyloCoalSimulations.jl
@@ -2,6 +2,8 @@ module PhyloCoalSimulations
 
 using Distributions
 import PhyloNetworks     # brings PhyloNetworks in scope, but none of its names
+import Random: AbstractRNG, default_rng
+
 const PN = PhyloNetworks # import PhyloNetworks as PN requires julia v1.6
 
 export
