4839b8afaabe500b62a3487dbc43d33882f7af6a
jjustison <justisnojo@gmail.com>
2024-11-22 18:02:51 -0500

    update read and write functions to align with PhyloNetworks 0.17

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,9 +1,9 @@
-net = readTopology("((C:0.9,(B:0.2)#H1:0.7::0.6):0.6,(#H1:0.6,A:1):0.5);");
+net = readnewick("((C:0.9,(B:0.2)#H1:0.7::0.6):0.6,(#H1:0.6,A:1):0.5);");
 PN.nameinternalnodes!(net, "i"); # "i" is a prefix to name internal nodes
 
 rng = StableRNG(7)
 
-tree = readTopology(writeTopology(simulatecoalescent(rng, net,1,1; nodemapping=true)[1]))
+tree = readnewick(writenewick(simulatecoalescent(rng, net,1,1; nodemapping=true)[1]))
 
 tree.node[1].name = "a name not in the species phylogeny" # give an incorrect name
 message = "The gene and species phylogeny have different sets of node names"
