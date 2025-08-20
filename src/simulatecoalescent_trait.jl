ad8c53116d9958312524f69291a18419c4c7b04a
Cecile Ane <cecileane@users.noreply.github.com>
2025-08-20 17:50:41 -0500

    store trait if scalar; return gene trees also; docstring

--- src/simulatecoalescent_trait.jl
+++ src/simulatecoalescent_trait.jl
@@ -1,16 +1,38 @@
 """
-simulate_locuseffect_at!(node::PN.Node, transition_distribution_family, P0_distribution)
-simulate_locuseffect_at!(childnode::PN.Node, transition_distribution_family, len, parenttrait)
-simulate_locuseffect_below!(node::PN.Node, transition_distribution_family)
-simulate_locuseffect!(Y, gtree::HybridNetwork, transition_distribution_family)
+function simulate_polygenictrait([rng::AbstractRNG,] net, nrep, nloci,
+    P0_distribution::Distribution, transition_distribution_family;
+    nindividuals=1, inheritancecorrelation=0.0)
+)
 
-warnings:
-- the nodes' field `booln5` is used to track which nodes have been simulated
-  already, as a sanity check. Not used for any other purpose, only temporarily.
-- no check that `Y` has the correct keys (individual names)
+Simulate a polygenetic trait controlled by multiple additive loci, each with
+effect `Yi`: `X = (Y_1+...+ Y_nloci)/sqrt(nloci)`.
+`nindividuals` is the number of individuals sampled from each taxon in `net`.
+It can be a single integer, or a dictionary as in [`simulatecoalescent`](@ref).
 
-todo:
-- argument to allow for correlated inheritance
+Each locus effect `Y` evolves along its own gene tree, sampled from the
+coalescent process, with possible correlated inheritance if
+`inheritancecorrelation > 0`: see [`simulatecoalescent`](@ref).
+Note that ancestors in some fixed ancestral population are
+different individuals across different loci.
+
+For each locus, `Y` at individuals in the origin population `P0`, taken to be
+the root of the network, is sampled from `P0_distribution`.
+Given state `y0` at the start of an edge in the gene tree,
+the trait value `yt` at the end of the edge is sampled from
+`transition_distribution_family(y0,t)` where `t` is the edge length.
+
+Output:`(X, tiplabels, genetrees)` where
+- `X` is a vector of matrices, of length `nrep`: one matrix for each independent
+  replicate. For replicate `i`, `X[i]` is of size `N×p` where `N` is the total
+  number of individuals and `p` the dimension of the trait.
+- `tiplabels` lists the tips labels in gene trees, in the order in which they
+  are considered in each trait matrix. That is: `X[i][j,:]` contains the traits
+  in replicate `i` for individual `tiplabels[j]`.
+- `genetrees` is a vector of gene trees, of length `nrep * nloci`.
+  The first `nloci` trees were used for the first replicate, the next `nloci`
+  trees were used for the second replicate, etc.
+  If the trait is a simple scalar, then the locus effect simulated at node `n`
+  in the locus tree is stored in `n.fvalue`. Other functions may erase this.
 
 # examples
 
@@ -23,7 +45,9 @@ julia> using Distributions; root_prior = NormalCanon(0, 100) # μ=0, σ²=1/100:
 
 julia> transition_distribution(x,t) = Normal(x, 0.1*sqrt(t))
 
-julia> simulate_polygenictrait(net, 2, 1, root_prior, transition_distribution)
+julia> x,lab,gt = simulate_polygenictrait(net, 2, 1, root_prior, transition_distribution);
+
+julia> DataFrame(species=lab, rep1=x[1][:], rep2=x[2][:])
 ```
 
 The next example uses a binary 0/1 trait at each locus, and a Markov transition
@@ -38,7 +62,7 @@ julia> function transition_distribution(x,t)
            return Binomial(1, minorprob)
        end;
 
-julia> simulate_polygenictrait(net, 1, 5, root_prior, transition_distribution)
+julia> x,lab,gt = simulate_polygenictrait(net, 1, 5, root_prior, transition_distribution);
 ```
 """
 function simulate_polygenictrait(net::PN.HybridNetwork, args...; kwargs...)
@@ -51,7 +75,8 @@ function simulate_polygenictrait(
     nloci::Integer,
     P0_distribution::Distribution,
     transition_kernel;
-    nindividuals=1
+    nindividuals=1,
+    inheritancecorrelation=0.0,
 )
     PN.nameinternalnodes!(net, "i")
     P0_name = PN.getroot(net).name
@@ -84,11 +109,12 @@ function simulate_polygenictrait(
     N = length(genetree_tiplabels) # total number of individuals
     T = eltype(P0_distribution)
     p = length(P0_distribution) # number of variables
+    storeY = size(P0_distribution) == () # 'yes' if trait is a simple scalar
     Y = Matrix{T}(undef, N, p)  # for temp storage
     X = [zeros(typeof(zero(T)/1), N, p) for _ in 1:nrep] # X will be rescaled
     # simulate all gene trees to avoid the overhead of doing it for each rep
     locustrees = simulatecoalescent(rng, net, nloci * nrep, nindividuals;
-        nodemapping=true)
+        nodemapping=true, inheritancecorrelation=inheritancecorrelation)
     # each 'locustree' has its nodes already listed in post-order: used below
     ilocus = 0
     for irep in 1:nrep
@@ -96,28 +122,54 @@ function simulate_polygenictrait(
             ilocus += 1
             tree = locustrees[ilocus]
             simulate_locuseffect!(rng, Y, lab2index, tree, P0_name,
-                P0_distribution, transition_kernel)
+                P0_distribution, transition_kernel, storeY)
             X[irep] .+= Y
         end
     end
     X ./= sqrt(nloci)
-    return X
+    return X, genetree_tiplabels, locustrees
 end
 
+"""
+    simulate_locuseffect!(
+        rng::AbstractRNG,
+        Y::Array,
+        lab2index::Dict,
+        gtree::PN.HybridNetwork,
+        P0_name::String,
+        P0_distribution::Distribution,
+        transition_distribution_family
+    )
+
+Simulate one trait along its gene tree `gtree`, starting at individuals in
+population named `P0_name` according to `P0_distribution`, and whose evolution,
+when starting at state `y0` after time `t`, follows distribution
+`transition_distribution_family(y0,t)`.
+
+`Y` stores the simulated trait at the leaves: `Y[i]` is the trait for leaf `i`,
+where `i` is some fixed index for the `leaf` node: `lab2index[leaf.name]`.
+
+notes:
+- the nodes' field `booln5` is used to track which nodes have been simulated
+  already. It is only used for a sanity check, only temporarily.
+- if the trait is a scalar `Float64`, then is it stored in the gene tree itself,
+  at each node in its field `.fvalue`.
+"""
 function simulate_locuseffect!(
     rng::AbstractRNG,
-    Y,
+    Y::Array,
     lab2index::Dict,
     gtree::PN.HybridNetwork,
-    P0_name,
+    P0_name::String,
     P0_distribution::Distribution,
     transition_kernel,
+    storeY::Bool,
 )
     for n in gtree.node # in post-order already
         # nodes at the origin population = nodes named P0_name
         if n.name == P0_name
             simulate_locuseffect_at!(rng, Y, lab2index, n,
-                P0_distribution, transition_kernel)
+                P0_distribution, transition_kernel, storeY)
         end
         if n.leaf
             n.booln5 || error("the trait for $(n.name) was not simulated")
@@ -133,16 +185,20 @@ function simulate_locuseffect_at!(
     node::PN.Node,
     P0_distribution::Distribution,
     transition_kernel,
+    storeY::Bool,
 )
     node.booln5 &&
         error("locus effect at node number $(node.number) was already simulated")
-    node.booln5 = true
     nodeY = Distributions.rand(rng, P0_distribution)
+    if storeY
+        node.fvalue = nodeY
+    end
+    node.booln5 = true
     if node.leaf
         Y[lab2index[node.name]] = nodeY
     else
         simulate_locuseffect_below!(rng, Y, lab2index, node,
-            transition_kernel, nodeY)
+            transition_kernel, nodeY, storeY)
     end
     return nothing
 end
@@ -154,16 +210,20 @@ function simulate_locuseffect_at!(
     transition_kernel,
     len,
     parentY,
+    storeY::Bool,
 )
     child.booln5 &&
         error("locus effect at node number $(child.number) was already simulated")
     childY = rand(rng, transition_kernel(parentY, len))
+    if storeY
+        child.fvalue = childY
+    end
     child.booln5 = true
     if child.leaf
         Y[lab2index[child.name]] = childY
     else
         simulate_locuseffect_below!(rng, Y, lab2index, child,
-            transition_kernel, childY)
+            transition_kernel, childY, storeY)
     end
     return nothing
 end
@@ -174,11 +234,12 @@ function simulate_locuseffect_below!(
     node::PN.Node,
     transition_kernel,
     nodeY,
+    storeY::Bool,
 )
     for ce in node.edge
         node ≡ PN.getparent(ce) || continue # loop through child edges only
         simulate_locuseffect_at!(rng, Y, lab2index, PN.getchild(ce),
-            transition_kernel, ce.length, nodeY)
+            transition_kernel, ce.length, nodeY, storeY)
     end
     return nothing
 end
